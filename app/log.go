438df2660fa83fdc9a78fdeaceea360b0e9da1cf
xiaoning.sun <xiaoning.sun@ele.me>
2019-12-21 15:01:28 +0800

    defaultTimeFormat

--- app/log.go
+++ app/log.go
@@ -2,17 +2,16 @@ package app
 
 import (
 	"focus/cfg"
+	timutil "focus/util/tim"
 	"github.com/scue/go-logrotate"
 	"github.com/sirupsen/logrus"
 )
-
-const DefaultTimeFormat = "2006-01-02 15:04:05.000"
 
 func InitLog() {
 	serverConfig := cfg.FocusCtx.Cfg.Server
 	logrus.SetLevel(logrus.DebugLevel)
 	logrus.SetFormatter(&logrus.JSONFormatter{
-		TimestampFormat: DefaultTimeFormat,
+		TimestampFormat: timutil.DefaultTimeFormat,
 	})
 	if serverConfig.Env == "prod" {
 		logrus.SetLevel(logrus.InfoLevel)
