0c8fb71449f15edd16893cdbbb61e22ce9a57c9d
xiaoning.sun <xiaoning.sun@ele.me>
2019-12-21 14:58:58 +0800

    update custom err

--- types/err.go
+++ types/err.go
@@ -13,14 +13,41 @@ const (
 )
 
 const (
-	Success           = 0
-	SystemError       = 9999
-	InvalidParamError = 9998
-	RepeatRequest     = 9997
-	NeedAuthError     = 9996
-	UserNotFound      = 5001
-	ExceedRateLimit   = 5002
+	Success              = 0
+	SystemError          = 9999
+	InvalidParamError    = 9998
+	RepeatRequest        = 9997
+	NeedAuthError        = 9996
+	DbError              = 9995
+	NotFound             = 5001
+	ExceedRateLimit      = 5002
+	DataDirty            = 5003
+	PayChannelNotSupport = 5004
 )
+
+func InvalidParamErr(msg string) error {
+	return NewErr(InvalidParamError, msg)
+}
+
+func SystemErr(msg string) error {
+	return NewErr(SystemError, msg)
+}
+
+func RepeatRequestErr(msg string) error {
+	return NewErr(RepeatRequest, msg)
+}
+
+func NeedAuthErr(msg string) error {
+	return NewErr(NeedAuthError, msg)
+}
+
+func NotFoundErr(msg string) error {
+	return NewErr(NotFound, msg)
+}
+
+func DbErr(err error) error {
+	return NewErr(DbError, err.Error())
+}
 
 func NewErr(code int, msg string) error {
 	return &FocusError{
