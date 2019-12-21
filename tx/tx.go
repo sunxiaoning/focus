e83823d8510643ad2d001d9c2f676aed6e6647da
xiaoning.sun <xiaoning.sun@ele.me>
2019-12-21 15:11:15 +0800

    tx manager

---
+++ tx/tx.go
@@ -0,0 +1,34 @@
+package tx
+
+import (
+	"context"
+	"focus/cfg"
+	"github.com/jinzhu/gorm"
+)
+
+type txManager struct {
+	tx *gorm.DB
+}
+
+func NewTxManager() *txManager {
+	return &txManager{cfg.FocusCtx.DB.Begin()}
+}
+
+type TFunRes interface{}
+type TFun func(ctx context.Context) (res TFunRes, err error)
+
+func (txManager *txManager) RunTx(ctx context.Context, tFun TFun) (res TFunRes, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			txManager.tx.Rollback()
+		}
+	}()
+	ctx = context.WithValue(ctx, "tx", txManager.tx)
+	res, err = tFun(ctx)
+	if err != nil {
+		txManager.tx.Rollback()
+	} else {
+		txManager.tx.Commit()
+	}
+	return res, err
+}
